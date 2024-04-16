03ef8746b6c6f4c2dde2a2acc920ec6b87052c89
thomasmendez <thomasmendez01@gmail.com>
2024-04-16 15:03:16 -0500

    Added axios request

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,11 +1,25 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
+import axios from 'axios'
 
 interface HomeProps {
   name: string
   jobTitle: string
-} 
+}
 
 const App: FC<HomeProps> = ({ name, jobTitle }) => {
+  useEffect(() => {
+    const getHome = async () => {
+        try {
+            const { data } = await axios.get(
+                'http://127.0.0.1:3000/api/v1/work',
+            )
+            console.log(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    getHome()
+  }, [])
   return (
     <>
       <h1>{name}</h1>

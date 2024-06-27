fc2fa5cf3e1b990a7ec64786b82d86744cd8f773
thomasmendez <thomasmendez01@gmail.com>
2024-06-27 00:52:44 -0500

    Added headers options

--- src/services/personalWebsiteApi.ts
+++ src/services/personalWebsiteApi.ts
@@ -7,38 +7,46 @@ export const baseUrl = import.meta.env.VITE_PERSONAL_WEBSITE_SERVICE
 
 const axiosInstance = axios.create()
 
+const options = {
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "*",
+    }
+}
+
 export const axiosGetWork = () => {
-    return axiosInstance.get(`${baseUrl}/api/v1/work`)
+    return axiosInstance.get(`${baseUrl}/api/v1/work`, options)
 }
 
 export const axiosPostWork = (newWork: Work) => {
-    return axiosInstance.post(`${baseUrl}/api/v1/work}`, newWork)
+    return axiosInstance.post(`${baseUrl}/api/v1/work}`, newWork, options)
 }
 
 export const axiosPutWork = (updateWork: Work) => {
-    return axiosInstance.put(`${baseUrl}/api/v1/work`, updateWork)
+    return axiosInstance.put(`${baseUrl}/api/v1/work`, updateWork, options)
 }
 
 export const axiosGetSkillsTools = () => {
-    return axiosInstance.get(`${baseUrl}/api/v1/skillsTools`)
+    return axiosInstance.get(`${baseUrl}/api/v1/skillsTools`, options)
 }
 
 export const axiosPostSkillsTools = (newSkillsTools: SkillsTools) => {
-    return axiosInstance.post(`${baseUrl}/api/v1/skillsTools}`, newSkillsTools)
+    return axiosInstance.post(`${baseUrl}/api/v1/skillsTools}`, newSkillsTools, options)
 }
 
 export const axiosPutSkillsTools = (newSkillsTools: SkillsTools) => {
-    return axiosInstance.put(`${baseUrl}/api/v1/skillsTools`, newSkillsTools)
+    return axiosInstance.put(`${baseUrl}/api/v1/skillsTools`, newSkillsTools, options)
 }
 
 export const axiosGetProjects = () => {
-    return axiosInstance.get(`${baseUrl}/api/v1/projects`)
+    return axiosInstance.get(`${baseUrl}/api/v1/projects`, options)
 }
 
 export const axiosPostProject = (newProject: Project) => {
-    return axiosInstance.post(`${baseUrl}/api/v1/project}`, newProject)
+    return axiosInstance.post(`${baseUrl}/api/v1/projects`, newProject, options)
 }
 
 export const axiosPutProject = (updateProject: Project) => {
-    return axiosInstance.put(`${baseUrl}/api/v1/project`, updateProject)
+    return axiosInstance.put(`${baseUrl}/api/v1/project`, updateProject, options)
 }

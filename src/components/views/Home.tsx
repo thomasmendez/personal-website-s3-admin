5bb90ae6c35f4964fe63a6b3201a759741d32c44
thomasmendez <thomasmendez01@gmail.com>
2024-07-09 11:33:53 -0500

    Added tailwind css and daisy ui

--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react"
+import { FC, useState } from "react"
+import Header from "../header/Header"
 
 interface HomeProps {
   name: string
@@ -6,10 +7,26 @@ interface HomeProps {
 }
 
 const HomeView: FC<HomeProps> = ({ name, jobTitle }) => {
+  const [darkMode, setDarkMode] = useState(false)
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
+
   return (
     <>
-      <h1>{name}</h1>
-      <h1>{jobTitle}</h1>
+      <Header/>
+      <div className={`${darkMode && "dark"}`}>
+        <main className="flex min-h-screen flex-col p-12 bg-neutral-100 dark:bg-neutral-900">
+          <h1 className="text-xl font-semibold dark:text-white">{name}</h1>
+          <h1>{jobTitle}</h1>
+        </main>
+        <button
+          className="absolute w-16 h-16 bottom-16 right-16 bg-neutral-900 dark:bg-white rounded-full text-white dark:text-black font-semibold"
+          onClick={toggleDarkMode}
+        >
+          {darkMode ? "LHT" : "Dark"}
+        </button>
+      </div>
     </>
   )
 }

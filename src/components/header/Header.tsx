e147ebaabd337fabf2effbba3f9926f029ce9f57
thomasmendez <thomasmendez01@gmail.com>
2024-07-09 11:54:18 -0500

    Refactored header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,25 +1,38 @@
-const Header = () => {
-    return(
-        <div className="bg-zinc-500 text-neutral-content p-4 flex justify-between items-center">
-          <div className="container mx-auto flex justify-between items-center">
-            <div className="text-xl normal-case">Thomas A. Mendez</div>
-            <div className="space-x-4 flex items-center">
-              <a href="/about" className="btn btn-ghost">About</a>
-              <a href="/work" className="btn btn-ghost">Work</a>
-              <a href="/skills-tools" className="btn btn-ghost">Skills & Tools</a>
-              <details className="relative group">
-                <summary className="btn btn-ghost">Projects</summary>
-                <ul className="text-zinc-500 absolute hidden space-y-2 group-hover:block bg-white shadow-lg p-2 rounded-lg">
-                  <li><a href="/software-engineering" className="btn btn-ghost">Software Engineering</a></li>
-                  <li><a href="/vr-ar" className="btn btn-ghost">VR / AR</a></li>
-                </ul>
-              </details>
-              <a href="/resume" className="btn btn-ghost">Resume</a>
-              <a href="/storybook" className="btn btn-ghost">Storybook</a>
-            </div>
-          </div>
+import { FC } from "react"
+
+interface HeaderProps {
+  darkMode: boolean
+  toggleDarkMode: () => void
+}
+
+const Header: FC<HeaderProps> = ({ darkMode, toggleDarkMode }) => {
+  return(
+    <div className="bg-zinc-500 text-neutral-content p-4 flex justify-between items-center">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-xl normal-case">Thomas A. Mendez</div>
+        <div className="space-x-4 flex items-center">
+          <a href="/about" className="btn btn-ghost">About</a>
+          <a href="/work" className="btn btn-ghost">Work</a>
+          <a href="/skills-tools" className="btn btn-ghost">Skills & Tools</a>
+          <details className="relative group">
+            <summary className="btn btn-ghost">Projects</summary>
+            <ul className="text-zinc-500 absolute hidden space-y-2 group-hover:block bg-white shadow-lg p-2 rounded-lg">
+              <li><a href="/software-engineering" className="btn btn-ghost">Software Engineering</a></li>
+              <li><a href="/vr-ar" className="btn btn-ghost">VR / AR</a></li>
+            </ul>
+          </details>
+          <a href="/resume" className="btn btn-ghost">Resume</a>
+          <a href="/storybook" className="btn btn-ghost">Storybook</a>
+          <button
+            className=" bg-neutral-900 dark:bg-white rounded-full text-white dark:text-black font-semibold"
+            onClick={toggleDarkMode}
+          >
+            {darkMode ? "LHT" : "Dark"}
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Header
--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -14,18 +14,12 @@ const HomeView: FC<HomeProps> = ({ name, jobTitle }) => {
 
   return (
     <>
-      <Header/>
+      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
       <div className={`${darkMode && "dark"}`}>
         <main className="flex min-h-screen flex-col p-12 bg-neutral-100 dark:bg-neutral-900">
           <h1 className="text-xl font-semibold dark:text-white">{name}</h1>
           <h1>{jobTitle}</h1>
         </main>
-        <button
-          className="absolute w-16 h-16 bottom-16 right-16 bg-neutral-900 dark:bg-white rounded-full text-white dark:text-black font-semibold"
-          onClick={toggleDarkMode}
-        >
-          {darkMode ? "LHT" : "Dark"}
-        </button>
       </div>
     </>
   )

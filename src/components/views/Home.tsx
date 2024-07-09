82d7b64acb0317d4c7a9b99f9b0fd6667719b871
thomasmendez <thomasmendez01@gmail.com>
2024-07-09 16:29:39 -0500

    Added sections for home page

--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from "react"
-import Header from "../header/Header"
+import Header from "../Header/Header"
 
 interface HomeProps {
   name: string
@@ -17,25 +17,39 @@ const HomeView: FC<HomeProps> = ({ name, jobTitle }) => {
       <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
       <div className={`${darkMode && "dark"}`}>
         <main className="flex min-h-screen text-center flex-col p-12 bg-neutral-100 dark:bg-neutral-900">
-          <h1>Image here</h1>
-          <h1 className="text-xl font-semibold dark:text-white">{name}</h1>
-          <h1 className="text-xl font-semibold dark:text-white">{jobTitle}</h1>
+          <section className="p-12 bg-neutral-100 dark:bg-neutral-800">
+            <h1>Image here</h1>
+            <h1 className="text-xl font-semibold dark:text-white">{name}</h1>
+            <h1 className="text-xl font-semibold text-zinc-500 dark:text-white">{jobTitle}</h1>
+          </section>
+          <section className="flex justify-center">
+            <section className="p-4">
+              <section>
+                <h1 className="text-xl font-semibold dark:text-white">About Me</h1>
+                <p>Details</p>
+              </section>
+              <section className="pt-4">
+                <h1 className="text-xl font-semibold dark:text-white">Education</h1>
+                <p>Education Info</p>
+                <p>Date</p>
+                <p>Cert</p>
+              </section>
+            </section>
+            <section className="p-4">
+              <h1 className="text-xl font-semibold dark:text-white">Contact</h1>
+            </section>
+          </section>
         </main>
-        <section>
-          <h1>About Me</h1>
-          <p>Details</p>
-        </section>
-        <section>
-          <h1>Education</h1>
-          <p>Education Info</p>
-          <p>Date</p>
-          <p>Cert</p>
-        </section>
-        <section>
-          <h1>Contact</h1>
-        </section>
-        <footer>
-          <p>Footer</p>
+        <footer className="flex justify-center">
+          <section className="flex justify-start">
+            <p>© 2024 Copyright</p>
+          </section>
+          <section className="flex justify-center">
+            <p>Links to different versions</p>
+          </section>
+          <section className="justify-end">
+            <p>contact info</p>
+          </section>
         </footer>
       </div>
     </>

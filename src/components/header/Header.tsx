4ca1c05a217b511802e4b721864e3c53c9dbf130
thomasmendez <thomasmendez01@gmail.com>
2024-07-09 14:09:32 -0500

    Adjusted header light and dark mode

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,28 +7,30 @@ interface HeaderProps {
 
 const Header: FC<HeaderProps> = ({ darkMode, toggleDarkMode }) => {
   return(
-    <div className="bg-zinc-500 text-neutral-content p-4 flex justify-between items-center">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-xl normal-case">Thomas A. Mendez</div>
-        <div className="space-x-4 flex items-center">
-          <a href="/about" className="btn btn-ghost">About</a>
-          <a href="/work" className="btn btn-ghost">Work</a>
-          <a href="/skills-tools" className="btn btn-ghost">Skills & Tools</a>
-          <details className="relative group">
-            <summary className="btn btn-ghost">Projects</summary>
-            <ul className="text-zinc-500 absolute hidden space-y-2 group-hover:block bg-white shadow-lg p-2 rounded-lg">
-              <li><a href="/software-engineering" className="btn btn-ghost">Software Engineering</a></li>
-              <li><a href="/vr-ar" className="btn btn-ghost">VR / AR</a></li>
-            </ul>
-          </details>
-          <a href="/resume" className="btn btn-ghost">Resume</a>
-          <a href="/storybook" className="btn btn-ghost">Storybook</a>
-          <button
-            className=" bg-neutral-900 dark:bg-white rounded-full text-white dark:text-black font-semibold"
-            onClick={toggleDarkMode}
-          >
-            {darkMode ? "LHT" : "Dark"}
-          </button>
+    <div className={`${darkMode && "dark"}`}>
+      <div className="text-neutral-content p-4 flex justify-between items-center bg-zinc-500 dark:bg-zinc-800">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="text-xl normal-case">Thomas A. Mendez</div>
+          <div className="space-x-4 flex items-center">
+            <a href="/about" className="btn btn-ghost">About</a>
+            <a href="/work" className="btn btn-ghost">Work</a>
+            <a href="/skills-tools" className="btn btn-ghost">Skills & Tools</a>
+            <details className="relative group">
+              <summary className="btn btn-ghost">Projects</summary>
+              <ul className="text-zinc-500 dark:text-zinc-300 absolute hidden space-y-2 group-hover:block shadow-lg p-2 rounded-lg bg-neutral-100 dark:bg-neutral-900">
+                <li><a href="/software-engineering" className="btn btn-ghost">Software Engineering</a></li>
+                <li><a href="/vr-ar" className="btn btn-ghost">VR / AR</a></li>
+              </ul>
+            </details>
+            <a href="/resume" className="btn btn-ghost">Resume</a>
+            <a href="/storybook" className="btn btn-ghost">Storybook</a>
+            <button
+              className="bg-neutral-900 dark:bg-white rounded-full text-white dark:text-black font-semibold"
+              onClick={toggleDarkMode}
+            >
+              {darkMode ? "LHT" : "Dark"}
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -16,10 +16,27 @@ const HomeView: FC<HomeProps> = ({ name, jobTitle }) => {
     <>
       <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
       <div className={`${darkMode && "dark"}`}>
-        <main className="flex min-h-screen flex-col p-12 bg-neutral-100 dark:bg-neutral-900">
+        <main className="flex min-h-screen text-center flex-col p-12 bg-neutral-100 dark:bg-neutral-900">
+          <h1>Image here</h1>
           <h1 className="text-xl font-semibold dark:text-white">{name}</h1>
-          <h1>{jobTitle}</h1>
+          <h1 className="text-xl font-semibold dark:text-white">{jobTitle}</h1>
         </main>
+        <section>
+          <h1>About Me</h1>
+          <p>Details</p>
+        </section>
+        <section>
+          <h1>Education</h1>
+          <p>Education Info</p>
+          <p>Date</p>
+          <p>Cert</p>
+        </section>
+        <section>
+          <h1>Contact</h1>
+        </section>
+        <footer>
+          <p>Footer</p>
+        </footer>
       </div>
     </>
   )

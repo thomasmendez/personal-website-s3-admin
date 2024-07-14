1aca8b0749f7841afc3ee72f1f46686339442f2e
Thomas Mendez <thomasmendez01@gmail.com>
2024-07-14 18:01:59 -0500

    Added icon component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,22 +1,25 @@
 import { FC, ReactNode } from 'react'
 
 interface Icon {
-  iconName: string
+  iconName?: string,
+  href: string,
   svgIcon: ReactNode
 }
 
-const Icon: FC<Icon> = ({ iconName, svgIcon }) => {
+const Icon: FC<Icon> = ({ iconName, svgIcon, href }) => {
   return(
     <div className="flex">
       <button
         type="button"
         className="rounded text-xs leading-normal pr-2"
       >
-        <span className="[&>svg]:h-5 [&>svg]:w-5 dark:[&>svg]:fill-[white]">
-          {svgIcon}
-        </span>
+        <a target="_blank" href={href}>
+            <span className="[&>svg]:h-5 [&>svg]:w-5 dark:[&>svg]:fill-[white]">
+              {svgIcon}
+            </span>
+        </a>
       </button>
-      <p className="text-black dark:text-white">{iconName}</p>
+      {iconName && <a target="_blank" href={href}><p className="text-black dark:text-white">{iconName}</p></a>}
     </div>
   )
 }

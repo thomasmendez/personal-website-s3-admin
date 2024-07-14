630cf6d4dceb6dfbba2c0acada37f5d007b80f5e
Thomas Mendez <thomasmendez01@gmail.com>
2024-07-13 22:46:47 -0500

    Updated about styling

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -16,11 +16,15 @@ const About: FC<AboutProps> = ({ aboutMeDetails, educationDetails, educationStar
           <p className="text-left text-xl font-semibold text-black dark:text-white">About Me</p>
           <p className="text-left text-black dark:text-white">I am a passionate software engineer who loves to solve real world problems using new unexplored technologies. I enjoy developing good user experiences, wether it be for desktop, mobile, or video games. Always looking forward to working on the next big project.</p>
         </section>
-        <section className="pt-4 grid-cols-1 space-y-2">
-          <p className="text-left text-xl font-semibold text-black dark:text-white">Education</p>
-          <div className="grid sm:flex md:grid-cols-4">
-            <p className="text-left col-span-3 text-black dark:text-white">Bachelor of Science in Arts and Entertainment Technologies, at The University of Texas at Austin</p>
-            <p className="sm:text-left col-span-1 md:text-right text-black dark:text-white">August 2016 - May 2019</p>
+        <section className="pt-4 space-y-2">
+          <p className="text-left text-xl grid-cols-12 font-semibold text-black dark:text-white">Education</p>
+          <div className="grid grid-cols-12">
+            <div className="text-left col-span-6 text-black dark:text-white">
+              Bachelor of Science in Arts and Entertainment Technologies, at The University of Texas at Austin
+            </div>
+            <div className="sm:text-left col-span-6 md:text-right text-black dark:text-white">
+              August 2016 - May 2019
+            </div>
           </div>
           <div>
             <ul className="text-left list-disc list-inside">

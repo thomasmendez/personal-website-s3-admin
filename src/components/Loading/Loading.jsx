a496ef67bad810302471b0c355038fd95389ea78
thomasmendez <thomasmendez01@gmail.com>
2024-07-18 16:11:36 -0500

    Fix file type

--- src/components/Loading/Loading.jsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-  return(
-    <div className="text-center">
-      <span className="min-h-80vh loading loading-spinner  text-neutral dark:text-accent"></span>
-    </div>
-  )
-}
-
-export default Loading
---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return(
+        <div className="text-center">
+            <span className="min-h-80vh loading loading-spinner  text-neutral dark:text-accent"></span>
+        </div>
+    )
+}
+
+export default Loading

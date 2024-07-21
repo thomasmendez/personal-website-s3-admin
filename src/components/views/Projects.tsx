8619dd99c5fcf5ea65d3f222d760f89fd2786560
thomasmendez <thomasmendez01@gmail.com>
2024-07-20 23:53:00 -0500

    Organized and styled projects page

--- src/components/views/Projects.tsx
+++ src/components/views/Projects.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from "react"
 import { getProjects } from "../../store/projectsApiSlice"
 import { AppDispatch } from "../../store/store"
 import { Project } from "../../types/projectTypes"
+import Ankhor from "../../assets/ankhor.mp4";
 import Loading from "../Loading/Loading"
 
 const ProjectsView = () => {
@@ -27,69 +28,88 @@ const ProjectsView = () => {
                 {projects.map((projects: Project, index: number) => (
                   <React.Fragment key={index}>
                     <section className="grid grid-cols-12 pt-4 pb-4 bg-neutral-200 dark:bg-neutral-900">
-                        <div className="col-start-3 col-span-7">
-                            <p className="text-xl font-bold">{projects.name}</p>
-                        </div>
                         <div className="col-start-3 col-span-3">
-                            <p className="underline">Project Description:</p>
-                            <p>{projects.description}</p>
-                        </div>
-                        <div className="flex col-start-3 col-span-3 space-x-1">
-                            <p className="underline">MyRole:</p>
-                            <p>{projects.role}</p>
-                        </div>
-                        <div className="col-start-3 col-span-3 space-x-5">
-                            <p className="underline">My Tasks:</p>
-                            <ul className="list-disc list-inside">
-                                {projects.tasks.map((task: string, listIndex: number) => (
-                                    <li key={listIndex}>{task}</li>
-                                ))}
-                            </ul>
-                        </div>
-                        {projects.teamSize !== null && (
-                            <div className="flex col-start-3 col-span-3 space-x-1">
-                                <p className="underline">Team Size:</p>
-                                <p>{projects.teamSize}</p>
+                            <div className="col-start-3 col-span-7">
+                                <p className="text-xl font-bold">{projects.name}</p>
                             </div>
-                        )}
-                        {projects.teamRoles !== null && (
-                            <div className="col-start-3 col-span-3 space-x-5">
-                                <p className="underline">Team Roles:</p>
+                            <div>
+                                <p className="underline">Project Description:</p>
+                                <p>{projects.description}</p>
+                            </div>
+                            <div className="flex space-x-1">
+                                <p className="underline">MyRole:</p>
+                                <p>{projects.role}</p>
+                            </div>
+                            <div className="space-x-5">
+                                <p className="underline">My Tasks:</p>
                                 <ul className="list-disc list-inside">
-                                    {projects.teamRoles.map((task: string, listIndex: number) => (
+                                    {projects.tasks.map((task: string, listIndex: number) => (
                                         <li key={listIndex}>{task}</li>
                                     ))}
                                 </ul>
                             </div>
-                        )}
-                        {projects.cloudServices !== null && (
-                            <div className="col-start-3 col-span-3 space-x-5">
-                                <p className="underline">Cloud Services:</p>
+                            {projects.teamSize !== null && (
+                                <div className="flex space-x-1">
+                                    <p className="underline">Team Size:</p>
+                                    <p>{projects.teamSize}</p>
+                                </div>
+                            )}
+                            {projects.teamRoles !== null && (
+                                <div className="space-x-5">
+                                    <p className="underline">Team Roles:</p>
+                                    <ul className="list-disc list-inside">
+                                        {projects.teamRoles.map((task: string, listIndex: number) => (
+                                            <li key={listIndex}>{task}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                            {projects.cloudServices !== null && (
+                                <div className="space-x-5">
+                                    <p className="underline">Cloud Services:</p>
+                                    <ul className="list-disc list-inside">
+                                        {projects.cloudServices.map((cloudService: string, listIndex: number) => (
+                                            <li key={listIndex}>{cloudService}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                            <div className="space-x-5">
+                                <p className="underline">Tools:</p>
                                 <ul className="list-disc list-inside">
-                                    {projects.cloudServices.map((cloudService: string, listIndex: number) => (
+                                    {projects.tools.map((cloudService: string, listIndex: number) => (
                                         <li key={listIndex}>{cloudService}</li>
                                     ))}
                                 </ul>
                             </div>
-                        )}
-                        <div className="col-start-3 col-span-3 space-x-5">
-                            <p className="underline">Tools:</p>
-                            <ul className="list-disc list-inside">
-                                {projects.tools.map((cloudService: string, listIndex: number) => (
-                                    <li key={listIndex}>{cloudService}</li>
-                                ))}
-                            </ul>
+                            <div className="flex space-x-1">
+                                <p className="underline">Project Duration:</p>
+                                <p>{projects.duration}</p>
+                            </div>
+                            <div className="flex space-x-1">
+                                <p className="underline">Project Date:</p>
+                                <p>{projects.startDate} - {projects.endDate}</p>
+                            </div>
+                            <div className="space-x-1">
+                                <p className="italic">*{projects.notes}*</p>
+                            </div>
                         </div>
-                        <div className="flex col-start-3 col-span-3 space-x-1">
-                            <p className="underline">Project Duration:</p>
-                            <p>{projects.duration}</p>
-                        </div>
-                        <div className="flex col-start-3 col-span-3 space-x-1">
-                            <p className="underline">Project Date:</p>
-                            <p>{projects.startDate} - {projects.endDate}</p>
-                        </div>
-                        <div className="col-start-3 col-span-3 space-x-1">
-                            <p className="italic">*{projects.notes}*</p>
+                        <div className="col-span-6">
+                            <div className="card bg-base-100 shadow-xl">
+                              <figure>
+                                <video controls>
+                                    <source src={Ankhor} type="video/mp4"/>
+                                    Your browser does not support the video tag
+                                </video>
+                              </figure>
+                              <div className="card-body">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                  <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                              </div>
+                            </div>
                         </div>
                         {/* <p>Category: {projects.category}</p>
                         <p>Duration: {projects.duration}</p>

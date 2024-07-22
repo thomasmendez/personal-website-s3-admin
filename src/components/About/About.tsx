7fb19144bbb24a0f8234c23168f3d0936d2d2120
thomasmendez <thomasmendez01@gmail.com>
2024-07-22 14:47:25 -0500

    Home about page layout fixed for mobile view

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -11,23 +11,21 @@ interface AboutProps {
 
 const About: FC<AboutProps> = ({ aboutMeDetails, educationDetails, educationStartDate, educationEndDate, degreeMinors }) => {
   return(
-    <section className="grid grid-cols-12 sm:gap-12 md:gap-22 bg-neutral-100 dark:bg-neutral-900">
-      <section className="p-4 col-start-3 col-span-7">
-        <section className="space-y-2">
+    <section className="grid grid-cols-12 bg-neutral-100 dark:bg-neutral-900">
+      <section className="sm:col-start-3 sm:col-span-7 col-span-12">
+        <section className="p-4 col-span-12 space-y-2">
           <p className="text-left text-xl font-semibold">About Me</p>
           <p className="text-left">I am a passionate software engineer who loves to solve real world problems using new unexplored technologies. I enjoy developing good user experiences, wether it be for desktop, mobile, or video games. Always looking forward to working on the next big project.</p>
         </section>
-        <section className="pt-4 space-y-2">
-          <p className="text-left text-xl grid-cols-12 font-semibold">Education</p>
-          <div className="grid grid-cols-12">
-            <p className="text-left col-span-6">
+        <section className="p-4 text-left col-span-12 space-y-2">
+          <p className="text-left col-span-12 text-xl grid-cols-12 font-semibold">Education</p>
+          <div className="col-span-12">
+            <p className="text-left">
               Bachelor of Science in Arts and Entertainment Technologies, at The University of Texas at Austin
             </p>
-            <p className="sm:text-left col-span-6 md:text-right">
+            <p className="text-left">
               August 2016 - May 2019
             </p>
-          </div>
-          <div>
             <ul className="text-left list-disc list-inside">
               <li>
                 Elements of Computing Certificate
@@ -36,7 +34,7 @@ const About: FC<AboutProps> = ({ aboutMeDetails, educationDetails, educationStar
           </div>
         </section>
       </section>
-      <section className="p-4 col-span-2 space-y-2">
+      <section className="p-4 sm:col-span-2 col-span-12 space-y-2">
         <p className="text-left text-xl font-semibold">Contact</p>
         <Icon
           iconName="LinkedIn"

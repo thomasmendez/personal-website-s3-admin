4a44c0dedd14f3b06fdce79f0df85403d9b868be
thomasmendez <thomasmendez01@gmail.com>
2024-07-22 15:30:18 -0500

    Updated Work page styling

--- src/components/views/Work.tsx
+++ src/components/views/Work.tsx
@@ -26,9 +26,8 @@ const WorkView = () => {
         if (work && work.length > 0) {
             content = <React.Fragment>
                 {work.map((employment: Work, index: number) => (
-                  <React.Fragment key={index}>
-                    <section className="grid grid-cols-12 pt-4 pb-4 bg-neutral-100 dark:bg-neutral-900">
-                        <div className="flex col-start-3 col-span-7 justify-between">
+                    <section key={index} className="grid grid-cols-12 pt-4 pb-4 bg-neutral-100 dark:bg-neutral-900">
+                        <div className="flex sm:col-start-3 sm:col-span-7 col-start-3 col-span-9 justify-between">
                             <div className="flex space-x-1">
                                 <p className="font-bold">{employment.jobTitle}</p>
                                 <p>at</p>
@@ -36,13 +35,13 @@ const WorkView = () => {
                                 <p>{employment.location.city}, {employment.location.state}</p>
                             </div>
                         </div>
-                        <div className="col-span-2 font-bold">
+                        <div className="sm:col-span-2 col-start-3 col-span-9 font-bold">
                             <p>{formatDateToMonthYear(employment.startDate)} - {formatDateToMonthYear(employment.endDate)}</p>
                         </div>
-                        <div className="col-start-3 col-span-9">
+                        <div className="sm:col-start-3 sm:col-span-9 col-start-3 col-span-9">
                             <p className="italic">{employment.jobRole}</p>
                         </div>
-                        <div className="col-start-3 col-span-9">
+                        <div className="sm:col-start-3 sm:col-span-9 col-start-3 col-span-9">
                             <ul className="list-disc list-inside">
                                 {employment.jobDescription.map((task: string, jobDescriptionIndex: number) => (
                                     <li key={jobDescriptionIndex}>{task}</li>
@@ -50,7 +49,6 @@ const WorkView = () => {
                             </ul>
                         </div>
                     </section>
-                  </React.Fragment>
                 ))}
             </React.Fragment>
         }

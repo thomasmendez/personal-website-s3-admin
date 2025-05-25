25f281aa3f0fbed4a397d363105d8c36bd6fbd1d
Thomas Mendez <thomasmendez01@gmail.com>
2025-05-25 15:14:05 -0500

    Have put request get from store

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,10 +1,14 @@
-// src/mocks/handlers.js
 import { http, HttpResponse } from 'msw'
+import { store } from '../store/store'
 import WorkMock from './__fixtures__/work'
 import SkillsToolsMock from './__fixtures__/skillsTools'
 import ProjectsMock from './__fixtures__/projects'
 export const baseUrl = import.meta.env.VITE_API_GATEWAY_ENDPOINT
- 
+
+// const getStoreState() {
+//   return store.getState()
+// }
+
 export const handlers = [
   // Intercept "GET https://example.com/user" requests...
   http.get('https://example.com/user', () => {
@@ -16,6 +20,9 @@ export const handlers = [
     })
   }),
 
+  // POST
+
+  // GET
   http.get(`${baseUrl}/api/v1/work`, () => {
     return HttpResponse.json(WorkMock)
   }),
@@ -26,5 +33,30 @@ export const handlers = [
 
   http.get(`${baseUrl}/api/v1/projects`, () => {
     return HttpResponse.json(ProjectsMock)
-  })  
+  }),
+
+  // PUT
+  http.put(`${baseUrl}/api/v1/work`, () => {
+    // const updatedIndex = store.getState().work.mode.indexOf('updated')
+    // const work = store.getState().work.entities[updatedIndex]
+    const work = "TODO"
+    console.log(JSON.stringify(work))
+    return HttpResponse.json(work)
+  }),
+
+  http.put(`${baseUrl}/api/v1/skillsTools`, () => {
+    const updatedIndex = store.getState().skillsTools.mode.indexOf('updated')
+    const skillsTools = store.getState().skillsTools.entities[updatedIndex]
+    console.log(JSON.stringify(skillsTools))
+    return HttpResponse.json(skillsTools)
+  }),
+
+  http.put(`${baseUrl}/api/v1/projects`, () => {
+    const updatedIndex = store.getState().projects.mode.indexOf('updated')
+    const projects = store.getState().projects.entities[updatedIndex]
+    console.log(JSON.stringify(projects))
+    return HttpResponse.json(projects)
+  }),
+
+  // DELETE
 ]

1b883e10f9d41c4389cf21307fac2a64fb440395
Thomas Mendez <thomasmendez01@gmail.com>
2025-05-25 21:40:52 -0500

    Add test pesudocode

---
+++ src/tests/e2e/skillsTools.spec.ts
@@ -0,0 +1,21 @@
+// TEST: Skill Tool View
+// TODO: Go to skills tool page
+// Ensure data from mock
+
+// TEST: Modify Skill Tool 
+// TODO: Go to skills tools page
+// Modify every field, save
+// Check that the fields were modified in the return statement
+
+// TEST: Add Skill Tool
+// go to skills tools page
+// modify newly added one
+// post request
+// check
+// verify in right order on screen
+
+// TEST: Remove skill tool
+// go to skills tool page
+// remove a existing skill tool
+// check
+// verify in right order on screen 

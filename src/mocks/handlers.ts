6d3413d532d63d95cbc87401af6eb875c00b1962
thomasmendez <thomasmendez01@gmail.com>
2025-05-26 14:58:27 -0500

    Add delete handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -10,16 +10,6 @@ export const baseUrl = import.meta.env.VITE_API_GATEWAY_ENDPOINT
 // }
 
 export const handlers = [
-  // Intercept "GET https://example.com/user" requests...
-  http.get('https://example.com/user', () => {
-    // ...and respond to them using this JSON response.
-    return HttpResponse.json({
-      id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
-      firstName: 'John',
-      lastName: 'Maverick',
-    })
-  }),
-
   // POST
 
   // GET
@@ -59,4 +49,18 @@ export const handlers = [
   }),
 
   // DELETE
+  http.delete(`${baseUrl}/api/v1/work`, () => {
+    console.log("deleted")
+    return HttpResponse.json(200)
+  }),
+
+  http.delete(`${baseUrl}/api/v1/skillsTools`, () => {
+    console.log("deleted")
+    return HttpResponse.json(200)
+  }),
+
+  http.delete(`${baseUrl}/api/v1/projects`, () => {
+    console.log("deleted")
+    return HttpResponse.json(200)
+  }),
 ]
